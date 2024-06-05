3edc88a556150996da373df91aad3c86115f4978
mklemmingen <MKL7543@duck.com>
2024-06-06 00:04:12 +0200

    created net proj around Program.cs MKL

--- Aufgabe3-Bildfaltung-C#/Bildfaltung/KernelFactory.cs
+++ Aufgabe3-Bildfaltung-C#/Bildfaltung/KernelFactory.cs
@@ -1,16 +1,16 @@
 public class KernelFactory
 {
-    public static Kernel CreateVerticalPrewittKernel()
+    public static Kernel CreatePrewittVertical()
     {
         // logic to create a vertical 3x3 Prewitt filter
     }
 
-    public static Kernel CreateHorizontalPrewittKernel()
+    public static Kernel CreatePrewittHorizontal()
     {
         // logic to create a horizontal 3x3 Prewitt filter
     }
 
-    public static Kernel CreateBoxFilter(int size)
+    public static Kernel CreateBox(int size)
     {
         // logic to create a square Box filter of the given size
     }
